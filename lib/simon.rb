8b2e4de22afd60d3912abc80864263f927e433ef
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-24 18:44:10 -0700

    implement simon

--- lib/simon.rb
+++ lib/simon.rb
@@ -4,27 +4,57 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    until @game_over
+      self.take_turn
+    end
+    
+    self.game_over_message
+    self.reset_game
+    
   end
 
   def take_turn
-
+    self.show_sequence
+    self.require_sequence
+    
+    unless @game_over
+      self.round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    self.add_random_color
+    @seq.each do |color|
+      puts color
+      sleep 1
+      system("clear")
+      sleep 0.25
+    end
   end
 
   def require_sequence
-
+    puts "Repeat the sequence"
+    @seq.each do |color|
+      user_color = gets.chomp
+      if color[0] != user_color
+        @game_over = true
+        break
+      end
+    end
+    sleep 0.25
   end
 
   def add_random_color
-
+    random_color = COLORS.sample
+    @seq.push(random_color)
+    
   end
 
   def round_success_message
@@ -36,6 +66,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
